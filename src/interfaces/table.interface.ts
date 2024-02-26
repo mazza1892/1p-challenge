29d022a87a1ce756e817d88fca3b15cc3f6c83a7
mazza1892 <michael@mazzamuto.co.uk>
2024-02-26 22:41:26 +0000

    Added basic logic to calculate days overall and rolling totals

---
+++ src/interfaces/table.interface.ts
@@ -0,0 +1,6 @@
+export interface TableInterface {
+    day: number;
+    month: string;
+    amount: number;
+    runningTotal: number;
+}
---
+++ src/services/calendar.service.ts
@@ -0,0 +1,97 @@
+import {TableInterface} from "../interfaces/table.interface.ts";
+
+/**
+ * CalendarService
+ * @class CalendarService
+ * @description Service for year related operations
+ * @export CalendarService
+ * @example import {CalendarService} from "./services/calendar.service.ts";
+ * @example CalendarService.isLeapYear(2020);
+ * @example CalendarService.isLeapYear(2021);
+ *
+ */
+export class CalendarService {
+    static daysArray: any[] = [];
+    static tableData: TableInterface[] = [];
+
+    /*
+     * Returns true if the year is a leap year, false otherwise.
+     * @param {number} year
+     * @returns {boolean}
+     */
+     public static isLeapYear(year: number): boolean {
+            if (year % 4 === 0) {
+                if (year % 100 === 0) {
+                    return year % 400 === 0;
+                }
+                return true;
+            }
+            return false;
+        }
+
+
+    /**
+     * Initializes the days of the year object
+     * @param year
+     */
+    public static initDaysOfTheYearObject(year: number): void {
+        let totalNumberOfDays = 365;
+        this.daysArray = [];
+
+        if(CalendarService.isLeapYear(year)) {
+            totalNumberOfDays = 366;
+        }
+
+        for(let i = 1; i <= totalNumberOfDays; i++) {
+            this.daysArray.push(i);
+        }
+    }
+
+
+    /**
+     * Returns the total projected savings
+     * @returns {number}
+     */
+    public static getTotalProjectedSavings(): number {
+        if(this.daysArray.length > 0) {
+            return this.daysArray.reduce((a, b) => a + b, 0) / 100;
+        }
+        return 0
+    }
+
+    /**
+     * Returns the number of days in a month
+     * @param date
+     * @returns {number}
+     */
+    public static getNumberOfDaysInMonth(date: Date): number {
+        let month = date.getMonth();
+        let year = date.getFullYear();
+        return new Date(year, month + 1, 0).getDate();
+    }
+
+    /**
+     * Populates the data array
+     * @param {Date} startDate
+     * @returns {TableInterface[]}
+     */
+    public static populateData(startDate: Date): TableInterface[] {
+        let data: TableInterface[] = [];
+        let runningTotal = 0;
+        this.daysArray.forEach((day) => {
+            runningTotal = runningTotal + day;
+            data.push({
+                day: day,
+                month: "January",
+                amount: day,
+                runningTotal: runningTotal
+            });
+        })
+
+        this.tableData = data;
+        return data;
+    }
+
+}
+
+
