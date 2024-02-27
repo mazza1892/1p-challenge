1e5683b2b0934f58bf12dad17ba2a2a90dda14b1
mazza1892 <michael@mazzamuto.co.uk>
2024-02-27 08:50:05 +0000

    CH-4: Updated styling for tableView

--- src/components/table-view/tableView.tsx
+++ src/components/table-view/tableView.tsx
@@ -3,28 +3,30 @@ import '../../interfaces/table.interface.ts';
 
 function TableView(props: any) {
     return (
-        <div className="mt-5">
-            <h2 className="text-4xl">Plan</h2>
-            <table className="w-full">
-                <thead>
-                    <tr>
-                        <th>Day</th>
-                        <th>Month</th>
-                        <th>Amount</th>
-                        <th>Running Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.data.map(function (item: any) {
-                        return <tr key={item.day}>
-                            <td>Day {item.day}</td>
-                            <td>{item.month}</td>
-                            <td>{props.currency}{item.day / 100}</td>
-                            <td>{props.currency}{item.runningTotal / 100}</td>
-                        </tr>;
-                    })}
-                </tbody>
-            </table>
+        <div className="h-96 overflow-y-scroll mt-5">
+            <div className="w-full">
+                <table className="w-full table-fixed">
+                    <thead className="sticky top-0 bg-emerald-950 text-white border-b">
+                        <tr>
+                            <th className="px-4 py-2 w-1/2">Day</th>
+                            <th className="px-4 py-2 w-1/2">Month</th>
+                            <th className="px-4 py-2 w-1/2">Amount</th>
+                            <th className="px-4 py-2 w-1/2">Running Total</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {props.data.map(function (item: any) {
+                            return <tr key={item.day} className="bg-gray-600 text-white">
+                                <td className="border px-4 py-2 w-1/2">Day {item.day}</td>
+                                <td className="border px-4 py-2 w-1/2">{item.month}</td>
+                                <td className="border px-4 py-2 w-1/2">{props.currency}{item.day / 100}</td>
+                                <td className="border px-4 py-2 w-1/2">{props.currency}{item.runningTotal / 100}</td>
+                            </tr>;
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
